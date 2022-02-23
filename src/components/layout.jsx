b51dc22b6b5aaa9c52325b51efbc9b3731c42c13
Christopher Straub <54823626+christopherstraub@users.noreply.github.com>
2022-02-22 21:54:22 -0600

    Create Header component template, rename file

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './header';
 
 export default function Layout({ children }) {
   return (
     <div>
-      This is the layout
+      <Header />
+      Layout
       {children}
     </div>
   );
